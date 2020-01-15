98a939fe598829715273ba48fa0e55a000f3f2b1
Fernando Garcia <fernandogdac@gmail.com>
2020-01-15 20:51:14 -0300

    aula 02
    trabalho com query
    o que sao bins
    filtrar apenas uma coluna

--- Introdução a Data Science.py
+++ Introdução a Data Science.py
@@ -3,10 +3,13 @@ import seaborn as sns
 
 
 notas = pd.read_csv('/Users/fernandogarcia/Documents/GitHub/Introduçao a Data Science/ml-1m/ratings.csv')
+filmes = pd.read_csv('/Users/fernandogarcia/Documents/GitHub/Introduçao a Data Science/ml-1m/movies.csv')
+
 
 notas.columns = ["usuarioId", "filmeId", "nota", "momento"]
+filmes.columns = ["filmeId", "titulo", "generos"]
 notas.nota.head()
 notas.nota.plot(kind='hist')
 notas.nota.describe()
 
-grafico = sns.boxplot(notas.nota)
+print(filmes)
